8f9f2c5225abfef17168baf60e337f1261e71776
Jesper Wistrom <theace4k@gmail.com>
2024-02-24 23:54:37 +0100

    Switch window splitting

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,23 @@
 <template>
-  <v-app>
-    <v-layout class="rounded rounded-md">
-      <v-app-bar title="Excel AI Helper"></v-app-bar>
+    <v-app>
+        <v-layout class="rounded rounded-md">
+            <v-app-bar app title="Excel AI Helper"></v-app-bar>
 
-      <v-navigation-drawer>
-        <SideBar />
-      </v-navigation-drawer>
-
-      <v-main class="d-flex flex-column align-center" style="height: 100vh;">
-        <ChatBox />
-  </v-main>
-
-    </v-layout>
-  </v-app>
+            <v-main>
+                <v-row no-gutters style="height: 100vh;">
+                    <v-col cols="4">
+                        <SideBar />
+                    </v-col>
+                    <v-col cols="8">
+                        <ChatBox />
+                    </v-col>
+                </v-row>
+            </v-main>
+        </v-layout>
+    </v-app>
 </template>
 
 <script setup lang="ts">
+
 </script>
+
